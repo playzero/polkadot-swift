c768c0802cb9250516f542a544f6f48115f0311b
2075 <marco@one.io>
2020-04-22 11:52:42 +0200

    Initial commit

---
+++ Example/polkadot/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  polkadot
+//
+//  Created by 2075 on 04/22/2020.
+//  Copyright (c) 2020 2075. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
